caa33dcb0e762914ff64f12faead53500053a09f
kms1572 <67312577+kms1572@users.noreply.github.com>
2020-09-07 16:43:45 +0900

    ㅅㅈㅇㄹ

--- Webmvc/src/kr/smhrd/MyUtil.java
+++ Webmvc/src/kr/smhrd/MyUtil.java
@@ -4,10 +4,9 @@ public class MyUtil {
 	// 1~ 10까지의 총합을 구하는 동작
 	public int hap() {
 		int sum = 0;
-		for (int i = 0; i <= 10; i++) {
+		for (int i = 0; i <= 100; i++) {
 			sum += 1;
 		}
-		// 상문초이 여기 좀 수정하시오...
 		return sum;
 	}
 }
